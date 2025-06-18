67d5b3100fe837a462f1e52c4849a17f4c1d292e
robmllze <86869208+robmllze@users.noreply.github.com>
2025-06-18 16:54:05 -0400

    +feat: Add UnsafenOrErrorAnnotation and UnsafeAnnotation annotations

---
+++ lib/src/annotations/unsafe_annotation.dart
@@ -0,0 +1,25 @@
+//.title
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Dart/Flutter (DF) Packages by dev-cetera.com & contributors. The use of this
+// source code is governed by an MIT-style license described in the LICENSE
+// file located in this project's root directory.
+//
+// See: https://opensource.org/license/mit
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//.title~
+
+const unsafeOrError = UnsafenOrErrorAnnotation();
+
+/// The class that enables the `@unsafeOrError` annotation.
+final class UnsafenOrErrorAnnotation {
+  const UnsafenOrErrorAnnotation();
+}
+
+const unsafe = UnsafeAnnotation();
+
+/// The class that enables the `@unsafe` annotation.
+final class UnsafeAnnotation {
+  const UnsafeAnnotation();
+}
